72fd9f137657ffda0c09aab1a168c62af661bda9
vikingmew <i@hanyang.ch>
2023-04-03 13:41:57 +0800

    inf

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -4,7 +4,7 @@ import numpy
 from Cython.Build import cythonize
 
 setup(
-  name = 'monotonic_align',
-  ext_modules = cythonize("core.pyx"),
-  include_dirs=[numpy.get_include()]
+    name="monotonic_align",
+    ext_modules=cythonize("core.pyx"),
+    include_dirs=[numpy.get_include()],
 )
